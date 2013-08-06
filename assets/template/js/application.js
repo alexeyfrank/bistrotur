fb36aa42a3d9ceee4e65304971c7c4f5fc837f0d
Alexey Frank <alexeyfrank@gmail.com>
2013-08-06 21:26:31 +0400

    add tours

--- assets/template/js/application.js
+++ assets/template/js/application.js
@@ -10,4 +10,18 @@ $(function() {
   var owl = $('.slider').data('owlCarousel');
 
   owl.play();
+
+
+  $('.tours .tour').hover(function() {
+    var height = $(this).height();
+    var fg = $(this).find('.foreground');
+    var abs = $(this).find('.abs');
+    abs.hide();
+    fg.hide().height(height).show();
+  }, function() {
+    var abs = $(this).find('.abs');
+    var fg = $(this).find('.foreground');
+    abs.show();
+    fg.hide();
+  });
 });
